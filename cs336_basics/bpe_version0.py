ccac15642d60e7ce9de3baa936c597957d5c2401
Japinder Singh <singh.japinder@gmail.com>
2026-01-11 06:06:13 +0000

    Fixes for bpe.

--- cs336_basics/bpe_version0.py
+++ cs336_basics/bpe_version0.py
@@ -2,7 +2,7 @@ from collections import Counter
 from dataclasses import dataclass
 from collections import defaultdict
 import os
-from typing import BinaryIO
+from typing import Any, BinaryIO, Tuple, Set, List, DefaultDict
 import regex as re
 from logging import Logger
 import logging
@@ -13,39 +13,84 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
-
-
-@dataclass
+@dataclass(frozen=True)
 class Token:
-    tokens: tuple[bytes]
+    value: bytes
 
 @dataclass
 class PreToken:
     raw: str
-    tokens: list[Token]
+    tokens: List[Token]
     count: int
 
-def process_pre_tokens(pre_tokens: Counter) -> tuple[Counter, dict[tuple[Token], set(PreToken)]]:
+TokenPair = Tuple[Token, Token]
+PairCounts = Counter[TokenPair]
+ReverseIndex = DefaultDict[TokenPair, Set[int]]
+
+def process_pre_tokens(pre_tokens: List[PreToken]) -> tuple[PairCounts, ReverseIndex]:
     """
     Goes through all the pre-tokens and does the following:
         1. Creates a counter of token pairs.
         2. Creates a reverse index from token pairs to pre-tokens.
     Args:
-        pre_tokens: A Counter of pre-tokens.
+        pre_tokens: A list of pre-tokens.
     Returns:
         A tuple containing the counter of token pairs and the reverse index.
     """
-    token_pairs = Counter()
-    reverse_index = defaultdict(set)
-    for k, v in pre_tokens.items():
-        pairs = zip(k.tokens, k.tokens[1:])
-        for pair in pairs:
-            token_pairs[pair] += v
-            reverse_index[pair].add(k)
+    token_pairs : PairCounts = Counter()
+    reverse_index : ReverseIndex = defaultdict(set)
+    
+    for i, pre_token in enumerate(pre_tokens):
+        for pair in zip(pre_token.tokens, pre_token.tokens[1:]):
+            token_pairs[pair] += pre_token.count
+            reverse_index[pair].add(i)
     return token_pairs, reverse_index
 
 
-def combine_tokens(token1: Token, token2: Token, token_pair_counts: Counter, reverse_index: dict[tuple[Token], set(PreToken)]):
+def get_merged_tokens(
+    pre_tokens: List[PreToken], 
+    combined_token: TokenPair, 
+    reverse_index: ReverseIndex) -> List[Tuple[int, List[Token]]]:
+    """
+    Given a token pair to combine, identfies all the pre-tokens containing the pair, and returns a list of tuples of the index of the pre-token and the new tokens.
+    Args:
+        pre_tokens: A list of pre-tokens.
+        combined_token: A token pair to combine.
+        reverse_index: A reverse index from token pairs to pre-tokens.
+    Returns:
+        A list of tuples of the index of the pre-token and the new tokens.
+    """
+    indices = reverse_index.get(combined_token)
+    if not indices:
+        # The combined token is not in the reverse index.
+        return []
+    
+    result = []
+    new_combined_token = Token(value=combined_token[0].value + combined_token[1].value)
+    for i in indices:
+        pre_token = pre_tokens[i]
+        old_tokens = pre_token.tokens
+        new_tokens = []
+        j = 0
+        while j < (len(old_tokens) - 1):
+            if combined_token == (old_tokens[j], old_tokens[j + 1]):
+                new_tokens.append(new_combined_token)
+                j += 2
+            else:
+                new_tokens.append(old_tokens[j])
+                j += 1
+        if j == len(old_tokens) - 1:
+            new_tokens.append(old_tokens[j])
+        
+        result.append((i, new_tokens))
+    return result
+    
+def combine_tokens(
+    pre_tokens: List[PreToken],
+    combined_token: Tuple[Token, Token], 
+    token_pair_counts: Counter[Tuple[Token, Token]], 
+    reverse_index: ReverseIndex
+) -> Tuple[Counter[Tuple[Token, Token]], ReverseIndex]:
     """
     Given a token pair, does the following:
         1. Updates the tokens for all pre-tokens containing the pair.
@@ -56,47 +101,42 @@ def combine_tokens(token1: Token, token2: Token, token_pair_counts: Counter, rev
         token2: The second token to combine.
         token_pair_counts: A counter of token pairs.
         reverse_index: A reverse index from token pairs to pre-tokens.
-    Returns:
+    Returns: 
         A tuple containing the counter of token pairs and the reverse index.
     """
-    combined_token = Token(token1.tokens + token2.tokens)
-    pre_tokens = reverse_index.get((token1, token2), set())
-    
-    for pre_token in pre_tokens:
-        for i in range(len(pre_token.tokens) - 1):
-            if (token1, token2) == (pre_token.tokens[i], pre_token.tokens[i + 1]):
-                # Replace with the combined token.
-                pre_token.tokens[i] = combined_token
-                pre_token.tokens[i + 1] = None
-            if i - 1 >= 0:
-                # Update count for [i - 1, i]
-                token_pair_counts[(pre_token.tokens[i - 1], pre_token.tokens[i])] += pre_token.count      
-                reverse_index[(pre_token.tokens[i - 1], pre_token.tokens[i])].add(pre_token)
-            if i + 2 < len(pre_token.tokens):
-                token_pair_counts[(pre_token.tokens[i], pre_token.tokens[i + 2])] += pre_token.count      
-                reverse_index[(pre_token.tokens[i], pre_token.tokens[i + 2])].add(pre_token)
+    merged_tokens = get_merged_tokens(pre_tokens, combined_token, reverse_index)
+    for i, new_tokens in merged_tokens:
+        pre_token = pre_tokens[i]
+        old_pairs = zip(pre_token.tokens, pre_token.tokens[1:])
+        for old_pair in old_pairs:
+            # Delete the old token pairs from the counter.
+            token_pair_counts[old_pair] -= pre_token.count
+            if token_pair_counts[old_pair] <= 0:
+                token_pair_counts.pop(old_pair, None)
+            s = reverse_index.get(old_pair)
+            if s is not None:
+                s.discard(i)
+                if not s:
+                    reverse_index.pop(old_pair, None)
         
-        # Remove none tokens.
-        pre_tokens.tokens = [t for t in pre_token.tokens if t is not None]
-    
-    # Remove the combined token from the reverse index and token pairs.     
-    del reverse_index[(token1, token2)]
-    del token_pair_counts[(token1, token2)]
-    
+        pre_token.tokens = new_tokens
+        for new_pair in zip(new_tokens, new_tokens[1:]):
+            token_pair_counts[new_pair] += pre_token.count
+            reverse_index[new_pair].add(i)
+        
     return token_pair_counts, reverse_index
-
     
-def pretokenize_document(document: str) -> Counter: 
+def pretokenize_document(document: str) -> Counter[str]: 
     """
     Given a document, return a counter of the pre-tokens in the document.
     
     Args:
         document (string): A document.
-    Returns (Counter):
+    Returns (Counter[PreToken]):
         A counter of the pre-tokens in the document.
     """
     PAT = r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-    tokens = Counter(re.finditer(PAT, document))
+    tokens = Counter(m.group(0) for m in re.finditer(PAT, document))
     return tokens
 
 
@@ -147,7 +187,7 @@ def find_chunk_boundaries(
     return sorted(set(chunk_boundaries))
 
 
-def get_pretoken_corpus_counts(documents: list[str]) -> Counter:
+def get_pretoken_corpus_counts(documents: list[str]) -> List[PreToken]:
     """
     Given a list of documents, return a counter of the pre-tokens in the documents.
     Args:
@@ -155,20 +195,25 @@ def get_pretoken_corpus_counts(documents: list[str]) -> Counter:
     Returns:
         A counter of the pre-tokens in the documents.
     """
-    counter = Counter()
+    counter: Counter[str] = Counter()
     for document in documents:
-        doc_counter = pretokenize_document(document)
-        counter += doc_counter
-    return counter
+        counter += pretokenize_document(document)
+    
+    pretokens: List[PreToken] = []
+    for k, v in counter.items():
+        b = k.encode("utf-8")
+        pretoken_tokens = [Token(value=bytes([x])) for x in b]
+        pretokens.append(PreToken(raw=k, tokens=pretoken_tokens, count=v))
+    return pretokens
 
-def get_max_count_token_pair(token_pair_counts: Counter) -> tuple[Token, Token]:
+def get_max_count_token_pair(token_pair_counts: Counter[Tuple[Token, Token]]) -> Tuple[Token, Token]:
     """
     Given a counter of token pairs, returns the token pair with the maximum count.
     """
-    max_count = token_pair_counts.most_common(1)[0][1]
-    # Tie break by lexicographic order.
-    max_count_pairs = [pair for pair, count in token_pair_counts.items() if count == max_count]
-    return max(max_count_pairs, key=lambda x: (x[0], x[1]))
+    if not token_pair_counts:
+        raise ValueError("Token pair counts is empty")
+    
+    return max(token_pair_counts.items(), key=lambda x: (x[1], x[0][0].value, x[0][1].value))[0]
 
 def get_documents(corpus_filename: str, num_processes: int, split_special_token: bytes) -> list[str]:
     """
@@ -181,7 +226,7 @@ def get_documents(corpus_filename: str, num_processes: int, split_special_token:
     Returns:
         A list of documents.
     """
-    with open(file_name, "rb") as f:
+    with open(corpus_filename, "rb") as f:
         boundaries = find_chunk_boundaries(f, num_processes, split_special_token)
         # The following is a serial implementation, but you can parallelize this
         # by sending each start/end pair to a set of processes.
@@ -193,11 +238,11 @@ def get_documents(corpus_filename: str, num_processes: int, split_special_token:
             documents.append(chunk)
         return documents
     
-def bpe_version_0(documents: list[str], end_of_text: str, num_merges: int):
-    vocabulary = [end_of_text] + bytes(range(256))
+def bpe_version_0(documents: list[str], end_of_text: bytes, num_merges: int):
+    vocabulary = [end_of_text] + [bytes([i]) for i in range(256)]
     
     # Corpus is split based on special tokens.
-    pretoken_counts = get_pretoken_corpus_counts(corpus_filename, num_merges, end_of_text)
+    pretoken_counts: List[PreToken] = get_pretoken_corpus_counts(documents)
     log.info(f"Pretoken counts: {pretoken_counts}")
     token_pair_counts, reverse_index = process_pre_tokens(pretoken_counts)
     log.info(f"Token pair counts: {token_pair_counts}")
@@ -208,8 +253,7 @@ def bpe_version_0(documents: list[str], end_of_text: str, num_merges: int):
         max_count_token_pair = get_max_count_token_pair(token_pair_counts)
         log.info(f"Max count token pair {i}: {max_count_token_pair}")
         rules.append(max_count_token_pair)
-        log.info(f"Combining tokens {i}: {max_count_token_pair[0]} and {max_count_token_pair[1]}")
-        token_pair_counts, reverse_index = combine_tokens(max_count_token_pair[0], max_count_token_pair[1], token_pair_counts, reverse_index)
+        token_pair_counts, reverse_index = combine_tokens(max_count_token_pair, token_pair_counts, reverse_index)
         log.info(f"Token pair counts {i}: {token_pair_counts}")
         log.info(f"Reverse index {i}: {reverse_index}")
         vocabulary.append(max_count_token_pair[0].tokens + max_count_token_pair[1].tokens)
